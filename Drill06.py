a89803eba8c193e4fdaf22e2e3b998f8a19857d5
minkeuncho <minkeun123@naver.com>
2023-10-01 00:40:54 +0900

    마우스 클릭시 잔상 남도록 draw_point()함수 추가

--- Drill06.py
+++ Drill06.py
@@ -5,39 +5,45 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    global x, y , points
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            points.append((x, y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
+def draw_point():
+    global points
+    for point in points:
+        hand_arrow.draw(point[0], point[1])
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+points = []
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand_arrow.draw(x, y)
+    draw_point()
+    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
-    frame = (frame + 1) % 8
+    #frame = (frame + 1) % 8
 
     handle_events()
 
 close_canvas()
-
-
-
-
