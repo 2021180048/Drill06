c22bf38184e797e378d87c68aafb750cb2c9d912
minkeuncho <minkeun123@naver.com>
2023-10-01 23:11:28 +0900

    handle_events()함수 수정하고 필요할 것 같은 여러가지 변수들 추가

--- Drill06.py
+++ Drill06.py
@@ -8,17 +8,17 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
-    global running
-    global x, y , points
+    global running, mouse_x, mouse_y, points, point_count
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            mouse_x, mouse_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
             points.append((x, y))
+            point_count += 1
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -30,9 +30,19 @@ def draw_point():
 
 
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+x, y = 0, 0
+mouse_x, mouse_y = 0, 0
+start_x, start_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+end_x, end_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 points = []
+point_count = 0
+start_point = 0
 frame = 0
+index = 0
+i = 0
+frame_speed = 0
+direction = 1
+
 hide_cursor()
 
 while running:
